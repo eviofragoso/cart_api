2713e598b6d11b96def931f2fe48b41de67398d7
Evio Junior <evio.junior@spezi.com.br>
2025-05-23 00:02:49 -0300

    Fix: fixed route in test and added config in development for test host acceptance

--- spec/requests/carts_spec.rb
+++ spec/requests/carts_spec.rb
@@ -2,15 +2,21 @@ require 'rails_helper'
 
 RSpec.describe "/carts", type: :request do
   pending "TODO: Escreva os testes de comportamento do controller de carrinho necessários para cobrir a sua implmentação #{__FILE__}"
-  describe "POST /add_items" do
+  describe "POST /add_item" do
     let(:cart) { create(:cart) }
     let(:product) { create(:product, name: "Test Product", price: 10.0) }
-    let!(:cart_item) { CartItem.create(cart: cart, product: product, quantity: 1) }
+    let!(:cart_item) { create(:cart_item, cart: cart, product: product, quantity: 1) }
 
     context 'when the product already is in the cart' do
+
+      # INFO: Added this, because the test is creating a cart to be used when the logic for the cart creation is implemented in a callback 
+      before do
+        allow_any_instance_of(ActionDispatch::Request).to receive(:session) { { cart_id: cart.id } }
+      end
+  
       subject do
-        post '/cart/add_items', params: { product_id: product.id, quantity: 1 }, as: :json
-        post '/cart/add_items', params: { product_id: product.id, quantity: 1 }, as: :json
+        post '/cart/add_item', params: { product_id: product.id, quantity: 1 }, as: :json
+        post '/cart/add_item', params: { product_id: product.id, quantity: 1 }, as: :json
       end
 
       it 'updates the quantity of the existing item in the cart' do
